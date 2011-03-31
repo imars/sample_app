20ee12f812dd8867a60a0c4ae330645cc0f2a06b
Ian Marshall <i.marshall@mac.com>
2011-03-31 22:42:55 +0100

    User model with password and User show page

---
+++ spec/factories.rb
@@ -0,0 +1,7 @@
+# By using the symbol ':user', we get the Factory Girl gem to simulate the User model.
+Factory.define :user do |user|
+  user.name                  "James Bond"
+  user.email                 "james.bond@gov.uk.co"
+  user.password              "foobar"
+  user.password_confirmation "foobar"
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,7 +4,12 @@ describe User do
   #pending "add some examples to (or delete) #{__FILE__}"
 
   before(:each) do
-    @attr = { :name => "Example User", :email => "user@example.com" }
+    @attr = { 
+      :name => "Example User", 
+      :email => "user@example.com",
+      :password => "foobar",
+      :password_confirmation => "foobar"
+    }
   end
 
   it "should create a new instance given valid attributes" do
@@ -16,7 +21,7 @@ describe User do
     no_name_user.should_not be_valid
   end
 
-  it "should reject name athat are too long" do
+  it "should reject name that are too long" do
     long_name = "a" * 51
     long_name_user = User.new(@attr.merge(:name => long_name))
     long_name_user.should_not be_valid
@@ -42,13 +47,78 @@ describe User do
       invalid_email_user.should_not be_valid
     end
   end
-
+       
   it "should reject duplicate email addresses" do
-    # Put a user with an email in the database
-    upcased_email = @attr[:email].upcase
-    User.create!(@attr.merge(:email => upcased_email))
-    user_with_duplicate_email = User.create!(@attr)
+    # Put a user with given email address into the database.
+    User.create!(@attr)
+    user_with_duplicate_email = User.new(@attr)
     user_with_duplicate_email.should_not be_valid
   end
 
+  it "should reject duplicate email addresses insensitive to case" do
+    # Put a user with an email in the database
+    upcased_email = @attr[:email].upcase
+    User.create!(@attr.merge(:email => upcased_email))
+    user_with_duplicate_email = User.new(@attr)
+    user_with_duplicate_email.should_not be_valid
+  end
+
+  describe "password validations" do
+    it "should require a password" do
+      User.new(@attr.merge(:password => "", :password_confirmation => "")).should_not be_valid
+    end
+
+    it "should require matching password confirmation" do
+      User.new(@attr.merge(:password_confirmation => "invalid")).should_not be_valid
+    end
+
+    it "should reject short passwords" do
+      short = "*" * 5
+      hash = @attr.merge(:password => short, :password_confirmation => short)
+      User.new(hash).should_not be_valid
+    end
+
+    it "should reject short passwords" do
+      long = "*" * 41
+      hash = @attr.merge(:password => long, :password_confirmation => long)
+      User.new(hash).should_not be_valid
+    end
+  end
+
+  describe "password encryption" do
+    before(:each) do
+      @user = User.create!(@attr)
+    end
+    it "should have an encrypted password attribute" do
+      @user.should respond_to(:encrypted_password)
+    end
+    it "should set the encrypted password" do
+      @user.encrypted_password.should_not be_blank
+    end
+  
+
+    describe "has_password? method" do
+      it "should be true if passwords match" do
+        @user.has_password?(@attr[:password]).should be_true
+      end
+      it "should be false if passwords don't match" do
+        @user.has_password?("invalid").should be_false
+      end
+    end
+
+    describe "authentication method" do
+      it "should return nil on password/email mismatch" do
+        wrong_password_user = User.authenticate(@attr[:email], "wrongpass")
+        wrong_password_user.should be_nil
+      end
+      it "should return nil for an email address with no user" do
+        non_existent_user = User.authenticate( "bar@foo.com", @attr[:password] )
+        non_existent_user.should be_nil
+      end
+      it "should return the user object on email/password match" do
+        matching_user = User.authenticate(@attr[:email], @attr[:password])
+        matching_user.should == @user
+      end
+    end
+  end 
 end
